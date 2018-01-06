e7b8693a7e05fde67b6c71cdd54898b5f4e27638
nla55 <nla55@sfu.ca>
2018-01-05 16:52:23 -0800

    final

--- weather_classifier.py
+++ weather_classifier.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 import sys
@@ -14,13 +12,18 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
 
 
 pd.options.mode.chained_assignment = None
 
 
-
 def find_time(filepath):
+    """
+    to extract time from the path of the image
+    :param filepath: the path of the image
+    :return: substring containing the time information
+    """
     pattern = re.compile(r'20\d+0000')
     m = pattern.search(filepath)
     if m:
@@ -30,19 +33,32 @@ def find_time(filepath):
 
 
 def format_time(string):
+    """
+    to format time to be in the form xxxx-xx-xx xx:00
+    :param string: substring containing the time information
+    :return: reformatted time string
+    """
     return string[:4] + '-' + string[4:6] + '-' + string[6:8] + ' ' + string[8:10] + ':00'
 
 
-# read from a folder and concatenate into a dataframe
 def read_csvs(path):
-    path = r'./weather'
+    """
+    to read all csv files from a folder and concatenate them into one dataframe
+    :param path: the location of the folder
+    :return: one dataframe containing records from all csv files
+    """
     files = glob.iglob(os.path.join(path, "*.csv"))
     weather = pd.concat((pd.read_csv(f, header=14) for f in files), ignore_index=True)
     return weather
 
 
-# read all the images in the folder into one big numpy array, return both this array and the series containin image names
 def read_images(path):
+    """
+    to read all the images in a folder into one big numpy array
+    :param path: the location of the folder
+    :return: images: one numoy array containing all the images
+             filenames: a pandas Series containing all image names
+    """
     files = glob.iglob(os.path.join(path, "*.jpg"))
     collection = []
     collection_names = []
@@ -56,6 +72,11 @@ def read_images(path):
 
 
 def plot_occurence(weather_column):
+    """
+    to plot the occurrence of original weather descriptions
+    :param weather_column: the Series containing weather descriptions for records
+    :return: None
+    """
     weather_column.value_counts().plot(kind='bar')
     plt.title('occurrence for each original weather category', fontsize='xx-large')
     plt.ylabel('occurrence', fontsize='xx-large')
@@ -64,6 +85,11 @@ def plot_occurence(weather_column):
 
 
 def cleaning(df):
+    """
+    to clean the dataframe such than the unique four weather categories are Rain, Clear, Cloudy and Snow
+    :param df: a dataframe containing the weather records
+    :return: cleaned dataframe
+    """
     df['Weather'] = df['Weather'].str.replace('Moderate ', '')
     df['Weather'] = df['Weather'].str.replace(' Showers', '')
     df['Weather'] = df['Weather'].str.replace('Mainly ', '')
@@ -76,6 +102,11 @@ def cleaning(df):
 
 
 def exclude_night(df):
+    """
+    to exclude weather records recorded at night and early morning.
+    :param df: a dataframe containing the weather records
+    :return: filtered new dataframe
+    """
     df = df[(df['Time'] != '06:00') & (df['Time'] != '07:00')]
     df = df[~((df['Month'] == 10) & (df['Time'] == '19:00'))]
     df = df[~(((df['Month'] == 11) | (df['Month'] == 12) | (df['Month'] == 1))
@@ -83,11 +114,31 @@ def exclude_night(df):
     return df
 
 
-def ml_analysis(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+def get_image_time(image_name):
+    """
+    to construct a dataframe containing formatted time information from image names
+    :param image_name: a dataframe containing image names
+    :return: a dataframe containing formatted time information for images
+    """
+    image_time = image_name.apply(find_time)
+    image_time = image_time.apply(format_time)
+    image_time = image_time.to_frame(name='time')
+    image_time.reset_index(inplace=True)
+    image_time.set_index('time', inplace=True)
+    return image_time
 
+
+def svc_analysis(X, y, pca_parameter):
+    """
+    to do a SVC analysis
+    :param X: training values
+    :param y: target values
+    :param pca_parameter: PCA parameter
+    :return: None
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
     model = make_pipeline(
-        PCA(1000),
+        PCA(pca_parameter),
         SVC(kernel='linear', C=0.1)
     )
     model.fit(X_train, y_train)
@@ -95,56 +146,81 @@ def ml_analysis(X, y):
     print(classification_report(y_test, model.predict(X_test)))
 
 
-def knn_analysis(X, y):
+def knn_analysis(X, y, pca_parameter):
+    """
+    to do a KNN analysis
+    :param X: training values
+    :param y: target values
+    :param pca_parameter: PCA parameter
+    :return: None
+    """
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-
     model = make_pipeline(
-        PCA(1500),
-        KNeighborsClassifier(n_neighbors=5),
+        PCA(pca_parameter),
+        KNeighborsClassifier(n_neighbors=8),
     )
     model.fit(X_train, y_train)
-    print("score for knn model:", model.score(X_test, y_test))
-    print(classification_report(y_test, model.predict(X_test)))
+    print("score for KNN model:", model.score(X_test, y_test))
+
+
+def gaussian_analysis(X, y, pca_parameter):
+    """
+    to do a GaussianNB analysis
+    :param X: training values
+    :param y: target values
+    :param pca_parameter: PCA parameter
+    :return: None
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    model = make_pipeline(
+        PCA(pca_parameter),
+        GaussianNB()
+    )
+    model.fit(X_train, y_train)
+    print("score for gaussianNB model:", model.score(X_test, y_test))
+
+
+def analysis(X, y, pca_parameter):
+    """
+    to use different machine learning models to do the analysis
+    :param X: training values
+    :param y: target values
+    :param pca_parameter: PCA parameter
+    :return: None
+    """
+    svc_analysis(X, y, pca_parameter)
+    # knn_analysis(X, y, pca_parameter)
+    # gaussian_analysis(X, y, pca_parameter)
+
 
 def main():
-    csv_path = r'./weather'
-    weather = read_csvs(csv_path)
-    weather = weather.dropna(axis=1, how='all')  # drop useless flag columns
+    csv_path = sys.argv[1]
+    image_path = sys.argv[2]
+    weather = read_csvs(csv_path)  # read all csv files in the folder
+    weather = weather.dropna(axis=1, how='all')  # drop empty flag columns
     weather = weather.drop('Data Quality', axis=1)
-
-    # read all the images in the folder into one big numpy array
-    image_path = r'./images'
-    images, filenames = read_images(image_path)
-
-    filenames = filenames.apply(find_time)
-    filenames = filenames.apply(format_time)
-    filenames = filenames.to_frame(name='time')
-    filenames.reset_index(inplace=True)
-    filenames.set_index('time', inplace=True)
-
     weather = exclude_night(weather)
+    images, filenames = read_images(image_path)  # read all images and image names in the folder
+    image_times = get_image_time(filenames)
 
-    weather_with_image = weather.join(filenames, on='Date/Time')
+    # join two dataframes to make the image index associated with corresponding weather record
+    weather_with_image = weather.join(image_times, on='Date/Time')
     weather_with_image = weather_with_image[pd.notnull(weather_with_image['index']) & pd.notnull(weather_with_image['Weather'])]
     weather_with_image['index'] = weather_with_image['index'].astype('int', copy=False)
 
-    new_weather = weather_with_image.filter(['Weather', 'index'])
+    new_weather = weather_with_image.filter(['Weather', 'index'])  # get a new dataframe containing only needed columns
     new_weather.reset_index(inplace=True)
     new_weather = new_weather.drop('level_0', axis=1)
     plot_occurence(new_weather['Weather'])
-
-    new_weather = cleaning(new_weather)
+    new_weather = cleaning(new_weather)  # do the cleaning on weather descriptions
     print(new_weather.groupby(new_weather['Weather']).count()['index'])
-    print(new_weather.shape)
 
     index_array = np.array(new_weather['index'])
-    used_images = images[index_array]
-    X = used_images.reshape(used_images.shape[0], -1)
+    used_images = images[index_array]  # filter out images which do not have corresponding weather records
+    X = used_images.reshape(used_images.shape[0], -1)  # flatten images
     y = new_weather.as_matrix(['Weather'])
     y = np.ravel(y)
-    ml_analysis(X, y)
-    #knn_analysis(X, y)
-
+    analysis(X, y, 1000)  # PCA parameter is 1000
 
 
 if __name__ == '__main__':
